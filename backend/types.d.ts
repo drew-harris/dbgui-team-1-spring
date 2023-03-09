14e430529a39425efd16c8eab5d9156fcd455bf2
Drew Harris <harrisd@smu.edu>
2023-03-09 10:09:17 -0600

    Format and eslint

--- backend/types.d.ts
+++ backend/types.d.ts
@@ -1,8 +1,10 @@
 declare namespace Express {
   export interface Request {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     user: any;
   }
   export interface Response {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     user: any;
   }
 }

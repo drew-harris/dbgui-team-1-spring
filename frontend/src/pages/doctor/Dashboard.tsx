a9c4e399cb2156b295145757334a78af4ca50b1b
Dembry123 <dylanembry1418@gmail.com>
2023-04-18 12:19:23 -0500

    made post component

--- frontend/src/pages/doctor/Dashboard.tsx
+++ frontend/src/pages/doctor/Dashboard.tsx
@@ -1,18 +1,23 @@
 import Button from "@mui/material/Button";
 import React, { useContext, useEffect } from "react";
 import { AuthContext } from "../../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
+
 
   useEffect(() => {
     console.log(user);
   }, [user]);
 
+
   return (
     <div>
       <h1>Doctor Dashboard</h1>
       <Button>Test MUI Button</Button>
+      <Button onClick={() => (navigate("/doctor/discussions"))}> Discussion Board</Button>
     </div>
   );
 }
--- frontend/src/pages/doctor/Discussions.tsx
+++ frontend/src/pages/doctor/Discussions.tsx
@@ -1,7 +1,39 @@
+import TextField from "@mui/material/TextField";
+import { useEffect, useState } from "react";
+import PostCard from "../../components/discussions/Postcard";
+import Stack from '@mui/joy/Stack';
+
+
 export default function Discussions() {
+  const [discussions, setDiscussions] = useState(null);
+
+  const fetchData = async () => {
+    const response = await fetch("http://localhost:8000/discussions/", {
+      headers: {
+        authorization: localStorage.getItem("jwt"),
+      },
+    });
+    const data = await response.json();
+
+    setDiscussions(data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <div>
-      <h1>Discussions</h1>
-    </div>
+    <main>
+      <div>
+        <h1>Discussions</h1>
+      </div>
+      <div>{JSON.stringify(discussions)}</div>
+      <PostCard/>
+      <div>
+        <form>
+          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        </form>
+      </div>
+    </main>
   );
 }

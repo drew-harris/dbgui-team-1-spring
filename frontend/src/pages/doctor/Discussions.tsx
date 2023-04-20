f7b2c2719a5fd1e986f7982b40e91ab4e4dffe0e
Dembry123 <dylanembry1418@gmail.com>
2023-04-19 19:06:50 -0500

    Update Discussions.tsx
    added button to go back to dashboard

--- frontend/src/pages/doctor/Discussions.tsx
+++ frontend/src/pages/doctor/Discussions.tsx
@@ -1,10 +1,12 @@
-import { Container, Typography, Card, CardContent, CardHeader, Grid } from '@mui/material';
+import { Container, Typography, Card, CardContent, CardHeader, Grid, Box, Button} from '@mui/material';
 import { useEffect, useState } from "react";
 import AddDiscussionForm from '../../components/discussions/AddPost';
 import axios from 'axios';
+import { useNavigate } from "react-router-dom";
 
 export default function Discussions() {
   const [discussions, setDiscussions] = useState([]);
+  const navigate = useNavigate();
 
   const fetchData = async () => {
     try{
@@ -24,26 +26,33 @@ export default function Discussions() {
   }, []);
 
   return (
-    <Container>
-      <Typography variant="h3" component="h1" gutterBottom>
-        Discussion Board
-      </Typography>
-      <AddDiscussionForm onAdd={fetchData} />
-      <Grid container spacing={3}>
-        {discussions.map((discussion) => (
-          <Grid item xs={12} key={discussion.id}>
-            <Card>
-              <CardHeader
-                title={discussion.title}
-                subheader={`Created by ${discussion.createdById} at ${discussion.createdAt}`}
-              />
-              <CardContent>
-                <Typography variant="body1">{discussion.body}</Typography>
-              </CardContent>
-            </Card>
-          </Grid>
-        ))}
-      </Grid>
-    </Container>
+    <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+      <Container sx={{ flex: '1 0 auto' }}>
+        <Typography variant="h3" component="h1" gutterBottom>
+          Discussion Board
+        </Typography>
+        <Grid container spacing={3}>
+          {discussions.map((discussion) => (
+            <Grid item xs={12} key={discussion.id}>
+              <Card>
+                <CardHeader
+                  title={discussion.title}
+                  subheader={`Created by ${discussion.createdById} at ${discussion.createdAt}`}
+                />
+                <CardContent>
+                  <Typography variant="body1">{discussion.body}</Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+      <Box sx={{ flexShrink: 0 }}>
+        <AddDiscussionForm onAdd={fetchData} />
+        <Button onClick={() => navigate("/doctor/dashboard")}>
+        Go to Dashboard
+        </Button>
+      </Box>
+    </Box>
   );
         }

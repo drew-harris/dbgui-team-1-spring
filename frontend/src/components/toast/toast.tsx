743124b6d083b1749fde5d513867173343b61d32
Cam <camdcurry@gmail.com>
2023-04-25 13:52:29 -0500

    toast

---
+++ frontend/src/components/toast/toast.tsx
@@ -0,0 +1,31 @@
+import { toast } from "react-hot-toast";
+
+export function ToastSuccess(message: string) {
+  toast.success(message, {
+    duration: 4000,
+    style: {
+      border: "1px solid #A855F7",
+      padding: "16px",
+      color: "#A855F7",
+    },
+    iconTheme: {
+      primary: "#A855F7",
+      secondary: "#FFFAEE",
+    },
+  });
+}
+
+export function ToastError(message: string) {
+  toast.error(message, {
+    duration: 4000,
+    style: {
+      border: "1px solid #EF4444",
+      padding: "16px",
+      color: "#EF4444",
+    },
+    iconTheme: {
+      primary: "#EF4444",
+      secondary: "#FFFAEE",
+    },
+  });
+}

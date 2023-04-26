e9e3623b9a8dc7d37617d7209d13d55c3680564c
Cam Curry <camdcurry@gmail.com>
2023-04-26 15:16:09 -0500

    push

---
+++ frontend/src/pages/patient/AddAppointmnet.tsx
@@ -0,0 +1,10 @@
+import NavBar from "../../components/nav/PatientNavBar";
+
+export default function AddAppointmnet() {
+  return (
+    <>
+      <NavBar />
+      <h1>Add Appointmnet</h1>
+    </>
+  );
+}
